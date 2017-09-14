b8f8053006c75f048b8ab482e294ce70135d5ad1
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-09-14 17:27:59 -0400

    Bug Fix

--- LargeStudyRoom.js
+++ LargeStudyRoom.js
@@ -3,48 +3,51 @@
 
 // Script begins //
 var totalhours = 3;
-var totalboxes = totalhours *2;
+var totalboxes = totalhours * 2;
 console.log("Welcome to Mudd Library 'Large Study Room' Automatic Reservation");
 var today = new Date();
 var year = today.getFullYear();
-var month = today.getMonth()+1;
-var day =today.getDate();
-console.log("Today's date: "+month+"/"+day+"/"+year);
+var month = today.getMonth() + 1;
+var day = today.getDate();
+console.log("Today's date: " + month + "/" + day + "/" + year);
 
 var page = require('webpage').create();
 page.open('http://northwestern.libcal.com/booking/mudd_large', function(status) {
-  if(status === "success") {
+  if (status === "success") {
     console.log("Status: " + status);
     // Successfully loaded the page
     // Calculate ID with algorithm
     // Automatically clicks all elements required
     console.log("Beginning to select elements");
-    for(var i=0;i<totalboxes;i++){ // Change totalhours to totalboxes
+    for (var i = 0; i < totalboxes; i++) { // Change totalhours to totalboxes
       page.evaluate(function() {
         var today = new Date();
         var year = today.getFullYear();
-        var month = today.getMonth()+1;
-        var day =today.getDate();
-        var calculateID = function(year,month,day){
-        // Algorithm that calculates elementID of needed squares to click
+        var month = today.getMonth() + 1;
+        var day = today.getDate();
+        var calculateID = function(year, month, day) {
+          // Algorithm that calculates elementID of needed squares to click
+          // ALGORITHM GOES HERE
 
-        var ans = 582002674;
-        console.log("Successfully calculated ID value");
-        return ans;
+
+          // ALGORITHM DONE RUNNING
+          var ans = 582002674;
+          console.log("Successfully calculated ID value");
+          return ans;
         };
 
-      var firstid = calculateID(year,month,day);
-      var targetid = firstid+i;
-      document.getElementById(targetid).click();
-});
+        var firstid = calculateID(year, month, day);
+        var targetid = firstid + i;
+        document.getElementById(targetid).click();
+      });
 
     }
     console.log("Successfully selected elements");
     //Submits form
 
     page.evaluate(function() {
-    document.getElementById("rm_tc_cont").click();
-});
+      document.getElementById("rm_tc_cont").click();
+    });
 
     // FILL OUT INFORMATION PAGE
     console.log("Filling out information...");
@@ -53,20 +56,21 @@ page.open('http://northwestern.libcal.com/booking/mudd_large', function(status) 
       var firstname = "Matthew";
       var lastname = "Wang";
       var email = "MatthewWang2020@U.northwestern.edu";
-      document.getElementById("fname").value=firstname;
-      document.getElementById("lname").value=lastname;
+      document.getElementById("fname").value = firstname;
+      document.getElementById("lname").value = lastname;
       document.getElementById("email").value = email;
-      document.getElementById('q1').value='Undergraduate student';
-});
+      document.getElementById('q1').value = 'Undergraduate student';
+    });
 
     console.log("Filled out all information, submitting reservation...");
     // Finished Running operations
     page.evaluate(function() {
-    document.getElementById("s-lc-rm-sub").click();
-});
+      document.getElementById("s-lc-rm-sub").click();
+    });
     console.log("Completed Reservation. Expect an email soon!");
     phantom.exit();
-  }
+}
+
   else{
     console.log("Encountered error, status !=== success");
     phantom.exit();
